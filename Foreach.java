e0f6962f2de90fb213baa57c7dda508f72f4d998
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-10 11:09:24 +0530

    Add files via upload

---
+++ Foreach.java
@@ -0,0 +1,8 @@
+public class Foreach{
+    public static void main(String args[]){
+        String a[]={"apple","orange","pineapple"};
+        for(String b:a)
+        System.out.print(b+" ");
+    }
+       
+}
