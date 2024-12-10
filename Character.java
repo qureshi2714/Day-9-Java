15be630a3799c52faa5101d08108c07fb8855962
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-10 10:59:02 +0530

    Add files via upload

---
+++ Character.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Character {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String a = sc . nextLine();
+        String res ="";
+        for(int i=0;i<a.length();i++)
+        res=a.charAt(i)+res;
+        System.out.println(res);
+    }
+    
+
+}
