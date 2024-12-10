5aec45f1bc3033f972e1740fba60309f1e892047
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-10 10:36:21 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+
+public class Hello{
+    public static void main(String args[]){
+        String a="Hello";
+        String b= new String("Hai");
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
+
