e290bcfd0ddc35a1997026b27d185783f2f9cd50
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-10 11:04:47 +0530

    Add files via upload

---
+++ Splitting.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Splitting {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        //Splitting a Sentences
+        String a=sc.nextLine();
+        String sp[]=a.split(" ");
+        System.out.println(sp);
+    }
+        
+}
